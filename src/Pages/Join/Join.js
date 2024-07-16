2530e4df44e0781e2b9e2ef9d60d95ac9fbd51e0
ProgrammerDavid1234 <olonadenifemi@gmail.com>
2024-07-16 10:39:31 +0100

    login

--- src/Pages/Join/Join.js
+++ src/Pages/Join/Join.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Join.module.css';
 import side from '../Assets/side.png';
-
+import Google from '../Assets/Icon-Google.png'
 const Join = () => {
   return (
     <div className={styles.joinContainer}>
@@ -12,8 +12,10 @@ const Join = () => {
         <input type="text" placeholder="Name" className={styles.inputField} />
         <input type="text" placeholder="Email or Phone Number" className={styles.inputField} />
         <input type="password" placeholder="Password" className={styles.inputField} />
+        <br />
         <button className="btn btn-danger" style={{ width: '100%' }}>Create Account</button>
-        <button className={styles.googleButton} style={{ width: '100%' }}>Sign up with Google</button>
+        <br />
+        <button className={styles.googleButton} style={{ width: '100%' }}> <img src={Google}  style={{height:'30px', paddingRight:'20px'}}/>Sign up with Google</button>
         <p>Already have an account? <a href="/login">Log in</a></p>
       </div>
     </div>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import styles from './Login.module.css';
+import side from '../Assets/side.png';
+import Google from '../Assets/Icon-Google.png';
 
 const Login = () => {
   return (
-    <div>
-      Login
+    <div className={styles.loginContainer}>
+      <img src={side} alt="Side" className={styles.sideImage} />
+      <div className={styles.formContainer}>
+        <h3 className={styles.headerText}>Log in to your account</h3>
+        <p className={styles.paragraphText}>Enter your credentials below</p>
+        <input type="text" placeholder="Email or Phone Number" className={styles.inputField} />
+        <input type="password" placeholder="Password" className={styles.inputField} />
+        <br />
+        <button className="btn btn-primary" style={{ width: '100%' }}>Log In</button>
+        <br />
+        <button className={styles.googleButton} style={{ width: '100%' }}>
+          <img src={Google} style={{ height: '30px', paddingRight: '20px' }} alt="Google" />
+          Log in with Google
+        </button>
+        <p>Don't have an account? <a href="/join">Sign up</a></p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
